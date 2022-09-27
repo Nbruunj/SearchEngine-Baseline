8f806cc3a39fea4067b431635e2cbc55ada64288
The Joker <54842770+Nbruunj@users.noreply.github.com>
2022-09-27 21:45:01 +0200

    en masse ting

--- ConsoleSearch/App.cs
+++ ConsoleSearch/App.cs
@@ -8,119 +8,158 @@ using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
+
 namespace ConsoleSearch
 {
     public class App
     {
-        public string API = "http://localhost:8071/search/search/";
+        public string API = "http://localhost:8071/prime/prime/";
+        
+
 
         public void Start()
         {
-            Console.WriteLine("press 1 to Console Search");
-            Console.WriteLine("press 2 prime number to check for primenumbers");
+            Console.WriteLine("1: do you wont to check a single prime number");
+            Console.WriteLine("2: do you wont to check multiple prime number");
             var select = Console.ReadLine();
-
             switch (select)
             {
                 case "1":
-                    var task = Run();
+                    var task = getstringprimenumber();
                     task.Wait();
+                   
                     break;
                 case "2":
+                    var task2 = getallprimesinbetween();
+                    task2.Wait();
                     break;
             }
-            
-            
-        }
 
-        public async Task Run()
-        {
 
-            Console.WriteLine("Console Search");
-            while (true)
-            {
-                Console.WriteLine("enter search terms - q for quit");
-                string input = Console.ReadLine() ?? string.Empty;
-                if (input.Equals("q")) break;
 
-                var wordIds = new List<int>();
-                var searchTerms = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                foreach (var t in searchTerms)
-                {
-                    var id = GetIdOf(t);
-                    if (id.Result != -1)
-                    {
-                        wordIds.Add(id.Result);
-                    }
-                    else
-                    {
-                        Console.WriteLine(t + " will be ignored");
-                    }
-                }
-
-                DateTime start = DateTime.Now;
-
-                var docIds = await GetDocuments(wordIds);
-                
-
-                // get details for the first 10             
-                var top10 = new List<int>();
-                foreach (var p in docIds.GetRange(0, Math.Min(10, docIds.Count)))
-                {
-                    top10.Add(p.Key);
-                }
-
-                TimeSpan used = DateTime.Now - start;
-
-                int idx = 0;
-
-                var docDetails = await GetDocumentDetails(top10);
-
-                foreach (var doc in docDetails)
-                {
-                    Console.WriteLine("" + (idx + 1) + ": " + doc + " -- contains " + docIds[idx].Value + " search terms");
-                    idx++;
-            
-                }
-                Console.WriteLine("Documents: " + docIds.Count + ". Time: " + used.TotalMilliseconds);
-                
-            }
-        }
-  
-        public async Task<int> GetIdOf(string word)
-        {
-            string url = string.Format(API + "getidof/" + word);
-            string json = await new WebClient().DownloadStringTaskAsync(url);
-            var result = JsonConvert.DeserializeObject<int>(json);
-            return result;
         }
         
-        public async Task<List<KeyValuePair<int, int>>> GetDocuments(List<int> wordIds)
-        {
-            var wordIdList = string.Join("&wordIds=", wordIds.Select(w => w.ToString()));
-            string url = string.Format(API + "getdocuments?wordIds=" + wordIdList);
-
-            WebClient client = new WebClient();
-
-            string json = await client.DownloadStringTaskAsync(url);
-            var results = JsonConvert.DeserializeObject<List<KeyValuePair<int, int>>>(json);
-
-            return results;
+         
+            public async Task<string> getstringprimenumber()
+            {
+            Console.WriteLine("enter number here thx you");
+            string input = Console.ReadLine() ?? string.Empty;
+            string url = string.Format(API + "CheckSinglePrimeNumber/" + input);
+            string json = await new WebClient().DownloadStringTaskAsync(url);
+            var result = JsonConvert.DeserializeObject<string>(json);
+            Console.WriteLine(result);
+            return result;
         }
 
-        public async Task<List<string>> GetDocumentDetails(List<int> docIds)
+        public async Task<List<string>> getallprimesinbetween()
         {
-            var docIdList = string.Join("&docIds=", docIds.Select(w => w.ToString()));
-            string url = string.Format(API + "getdocumentdetails?docIds=" + docIdList);
+            Console.Write("enter the first number");
+            string inputone = Console.ReadLine() ?? string.Empty;
+            Console.Write("enter the first number");
+            string inputtwo = Console.ReadLine() ?? string.Empty;
+            string url = string.Format(API + "CheckMultiplePrimeNumbers/" + inputone + "/" + inputtwo);
+            string json = await new WebClient().DownloadStringTaskAsync(url);
+            var result = JsonConvert.DeserializeObject<List<string>>(json);
+            foreach(var i in result)
+            {
+                Console.WriteLine(i);
+            }
+            
+            return result;
 
-            WebClient client = new WebClient();
-
-            string json = await client.DownloadStringTaskAsync(url);
-            var results = JsonConvert.DeserializeObject<List<string>>(json);
-
-            return results;
         }
 
 
-    }
+
+            /*
+            public async Task Run()
+            {
+
+                Console.WriteLine("Console Search");
+                while (true)
+                {
+                    Console.WriteLine("enter search terms - q for quit");
+                    string input = Console.ReadLine() ?? string.Empty;
+                    if (input.Equals("q")) break;
+
+                    var wordIds = new List<int>();
+                    var searchTerms = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var t in searchTerms)
+                    {
+                        var id = GetIdOf(t);
+                        if (id.Result != -1)
+                        {
+                            wordIds.Add(id.Result);
+                        }
+                        else
+                        {
+                            Console.WriteLine(t + " will be ignored");
+                        }
+                    }
+
+                    DateTime start = DateTime.Now;
+
+                    var docIds = await GetDocuments(wordIds);
+
+
+                    // get details for the first 10             
+                    var top10 = new List<int>();
+                    foreach (var p in docIds.GetRange(0, Math.Min(10, docIds.Count)))
+                    {
+                        top10.Add(p.Key);
+                    }
+
+                    TimeSpan used = DateTime.Now - start;
+
+                    int idx = 0;
+
+                    var docDetails = await GetDocumentDetails(top10);
+
+                    foreach (var doc in docDetails)
+                    {
+                        Console.WriteLine("" + (idx + 1) + ": " + doc + " -- contains " + docIds[idx].Value + " search terms");
+                        idx++;
+
+                    }
+                    Console.WriteLine("Documents: " + docIds.Count + ". Time: " + used.TotalMilliseconds);
+
+                }
+            }
+
+            public async Task<int> GetIdOf(string word)
+            {
+                string url = string.Format(API + "getidof/" + word);
+                string json = await new WebClient().DownloadStringTaskAsync(url);
+                var result = JsonConvert.DeserializeObject<int>(json);
+                return result;
+            }
+
+            public async Task<List<KeyValuePair<int, int>>> GetDocuments(List<int> wordIds)
+            {
+                var wordIdList = string.Join("&wordIds=", wordIds.Select(w => w.ToString()));
+                string url = string.Format(API + "getdocuments?wordIds=" + wordIdList);
+
+                WebClient client = new WebClient();
+
+                string json = await client.DownloadStringTaskAsync(url);
+                var results = JsonConvert.DeserializeObject<List<KeyValuePair<int, int>>>(json);
+
+                return results;
+            }
+
+            public async Task<List<string>> GetDocumentDetails(List<int> docIds)
+            {
+                var docIdList = string.Join("&docIds=", docIds.Select(w => w.ToString()));
+                string url = string.Format(API + "getdocumentdetails?docIds=" + docIdList);
+
+                WebClient client = new WebClient();
+
+                string json = await client.DownloadStringTaskAsync(url);
+                var results = JsonConvert.DeserializeObject<List<string>>(json);
+
+                return results;
+            }
+            */
+
+        }
 }
--- Loadbalancing/LoadBalancerStrategy.cs
+++ Loadbalancing/LoadBalancerStrategy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RoundRobin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,17 @@ namespace Loadbalancing
 {
     public class LoadBalancerStrategy : ILoadBalancerStrategy
     {
+
+       
         public List<string> services;
         public string API = "http://localhost:8071/prime/prime/";
-        public string API1 = "http://localhost:8071/prime/prime/";
-        public string API2 = "http://localhost:8071/prime/prime/";
+        public string API1 = "http://localhost:8072/prime/prime/";
+        public string API2 = "http://localhost:8073/prime/prime/";
+        public int apirobinhood = 0;
         public LoadBalancerStrategy() 
         {
+            
+
             services.Add(API);
             services.Add(API1);
             services.Add(API2);
@@ -21,6 +27,29 @@ namespace Loadbalancing
 
         public string NextService(List<string> services)
         {
+            if(apirobinhood == 3)
+            {
+                 apirobinhood = 0;
+            }
+            if (apirobinhood == 0)
+            {
+                apirobinhood++;
+                return services[0];
+                
+            }
+            else if(apirobinhood == 1)
+            {
+                apirobinhood++;
+                return services[1];
+                    
+
+            }
+            else if(apirobinhood == 2)
+            {
+                apirobinhood++;
+                return services[2];
+            }
+            
             
             throw new NotImplementedException();
         }
